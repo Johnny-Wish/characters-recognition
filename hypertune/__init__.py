a1c496174abd02b351bcc8f86fc8c499ff64a9f5
Shuheng Liu <wish1104@outlook.com>
2018-12-16 22:54:00 +0800

    add instructions for package `hypertune`

--- hypertune/__init__.py
+++ hypertune/__init__.py
@@ -8,5 +8,7 @@ GitHub:    https://github.com/Johnny-Wish
 (c) All Rights Reserved
 License: https://choosealicense.com/licenses/gpl-3.0/
 
-
+A tool package for hypertuning
+After creating a package under the `sklearn_models` (see `sklearn_models/__init__.py' for detailed help),
+run from  project root `python -m hypertune.py --<arg_names> <arg_values>`.
 """
