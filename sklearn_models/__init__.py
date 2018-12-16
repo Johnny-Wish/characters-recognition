c790bd204f79a1db1c86bddc06ed28a45fb2e00e
Shuheng Liu <wish1104@outlook.com>
2018-12-16 22:48:03 +0800

    add instructions for package `sklearn_models`

--- sklearn_models/__init__.py
+++ sklearn_models/__init__.py
@@ -8,5 +8,8 @@ GitHub:    https://github.com/Johnny-Wish
 (c) All Rights Reserved
 License: https://choosealicense.com/licenses/gpl-3.0/
 
-
+Package for storing sklearn models (classifiers)
+Hypertune your custom model by creating a module under the package, make sure to include a sklearn estimator (
+preferably named `model`) and a dict containing parameters and intervals (preferably named `parameter_distribution).
+These two models must be immediately accessible from an external python script. Check `neural_net_adam.py` for example.
 """
