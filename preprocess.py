e146fb37bb5421f6d9e6b4dc22353b469026a86d
Shuheng Johnny Liu <wish1104@outlook.com>
2018-12-16 14:34:17 +0800

    added files for preprocessing data in .mat format

---
+++ preprocess.py
@@ -0,0 +1,15 @@
+"""
+Created:   Dec 16, 2018
+encoding:  UTF-8
+Author:    Shuheng Liu
+Contact:   wish1104@outlook.com
+GitHub:    https://github.com/Johnny-Wish
+
+(c) All Rights Reserved
+License: https://choosealicense.com/licenses/gpl-3.0/
+
+This file contains pre-processing tools of the dataset, assumed to be of a standard .mat format
+"""
+
+from scipy.io import loadmat
+
