8ce142507d5e5d6a90330094697bd89ef0167efd
Shuheng Johnny Liu <wish1104@outlook.com>
2018-12-19 22:33:52 +0800

    write json metrics after searching and testing

--- hypertune/__main__.py
+++ hypertune/__main__.py
@@ -1,10 +1,9 @@
 import os
 import argparse
 from preprocess import Dataset
-from global_utils import dump
+from global_utils import dump, JsonMetricQueueWriter
 from .search_session import SearchSession
 from .reflexive_import import ReflexiveImporter
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -38,3 +37,11 @@ if __name__ == '__main__':
     for metric in session.test_result:
         print("testing {}: {}".format(metric, session.test_result[metric]))
     dump(session.test_result, os.path.join(opt.outf, "test-results.pkl"))
+
+    session.search_results.sort_values(by=["mean_test_score"])
+    JsonMetricQueueWriter("mean-f1-score", session.search_results.mean_test_score).write()
+    JsonMetricQueueWriter("std-f1-score", session.search_results.std_test_score).write()
+    JsonMetricQueueWriter("mean-fit-time", session.search_results.mean_fit_time).write()
+    JsonMetricQueueWriter("std-fit-time", session.search_results.std_fit_time).write()
+    JsonMetricQueueWriter("mean-score-time", session.search_results.mean_score_time).write()
+    JsonMetricQueueWriter("std-score-time", session.search_results.std_score_time).write()
