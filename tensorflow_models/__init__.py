8f669a5875208a78e6b45b0e4dfca776a0c812f8
Shuheng Liu <wish1104@outlook.com>
2018-12-25 18:35:01 +0800

    initialize the package `tensorflow_models`

---
+++ tensorflow_models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Created:   Dec 25, 2018
+E*ncoding:  UTF-8
+Author:    Shuheng Liu
+Contact:   wish1104@outlook.com
+GitHub:    https://github.com/Johnny-Wish
+
+(c) All Rights Reserved
+License: https://choosealicense.com/licenses/gpl-3.0/
+
+This package contains tensorflow model objects
+"""
