aba0a4b7279c08b8bd1831017e1799b4905cccec
Shuheng Johnny Liu <wish1104@outlook.com>
2019-01-10 20:59:59 +0800

    add a package for pytorch models

---
+++ pytorch_models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Created:   Jan 10, 2019
+E*ncoding:  UTF-8
+Author:    Shuheng Liu
+Contact:   wish1104@outlook.com
+GitHub:    https://github.com/Johnny-Wish
+
+(c) All Rights Reserved
+License: https://choosealicense.com/licenses/gpl-3.0/
+
+This is a package for pytorch implementation of image classifiers.
+"""
