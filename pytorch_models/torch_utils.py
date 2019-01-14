6fcfb96dc8bf14cf99b3d2728db31011a28d321d
Shuheng Johnny Liu <wish1104@outlook.com>
2019-01-15 01:21:08 +0800

    specify the mode for array-to-image transformation

--- pytorch_models/torch_utils.py
+++ pytorch_models/torch_utils.py
@@ -6,10 +6,11 @@ class ArrayTransform:
     """
     A callable that transforms  np.ndarray`s as if they are images
     """
+
     def __init__(self, img_transform):
         self.img_transform = img_transform
 
-    def __call__(self, x):
-        image = Image.fromarray(x, "L")
+    def __call__(self, x, mode="L"):
+        image = Image.fromarray(x, mode)
         resized = self.img_transform(image)
         return np.stack([np.asarray(resized, dtype=np.double)], axis=0)
