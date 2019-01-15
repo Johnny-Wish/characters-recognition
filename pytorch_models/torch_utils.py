e9275a767738b6a5cc0fb5f4c1fc3f3937a0c308
Shuheng Liu <wish1104@outlook.com>
2019-01-15 11:48:19 +0800

    add a tool function for computing metrics

--- pytorch_models/torch_utils.py
+++ pytorch_models/torch_utils.py
@@ -1,5 +1,7 @@
+import torch
 import numpy as np
 from PIL import Image
+from sklearn.metrics import precision_recall_fscore_support
 
 
 class ArrayTransform:
@@ -14,3 +16,21 @@ class ArrayTransform:
         image = Image.fromarray(x, mode)
         resized = self.img_transform(image)
         return np.stack([np.asarray(resized, dtype=np.double)], axis=0)
+
+
+def get_metrics_dict(labels: torch.Tensor, preds: torch.Tensor):
+    acc = (preds == labels).float().mean()
+    preds, labels = preds.cpu(), labels.cpu()
+    precision, recall, fscore, _ = precision_recall_fscore_support(labels, preds, average="micro")
+    _, _, _, support = precision_recall_fscore_support(labels, preds, warn_for=())
+    std_support = np.std(support)
+
+    # torch.Tensor are not serializable, therefore casting metrics to built-in python classes
+    return {
+        "accuracy": float(acc),
+        "precision": float(precision),
+        "recall": float(recall),
+        "fscore": float(fscore),
+        "std_support": float(std_support),
+    }
+
