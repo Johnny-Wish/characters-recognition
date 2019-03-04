3f8072fb69eb6525543856f7ab5120a7fc2ee197
Shuheng Liu <wish1104@outlook.com>
2019-03-05 01:03:58 +0800

    implement `resize`, `set_width` and `set_height` to manipulate self.fig

--- visual.py
+++ visual.py
@@ -18,7 +18,40 @@ class BaseVisualizer:
         """
         plt.figure(self.num)
 
-    def plot(self, force_replot=False):
+    def resize(self, size0, size1=None):
+        """
+        set the width and height of the figure attached to this instance
+        :param size0: width, or a tuple of (width, height) in inches
+        :param size1: height, or None (if size0 is a tuple)
+        :return: self
+        """
+        if size1 is None:
+            size0, size1 = size0
+        else:
+            size0, size1 = size0, size1
+        return self.set_width(size0).set_height(size1)
+
+    def set_width(self, width):
+        """
+        set the width of the figure attached to this instance
+        :param width: width in inches
+        :return: self
+        """
+        if width is not None:
+            self.fig.set_figwidth(width)
+        return self
+
+    def set_height(self, height):
+        """
+        set the height of the figure attached to this instance
+        :param height: height in inches
+        :return: self
+        """
+        if height is not None:
+            self.fig.set_figheight(height)
+        return self
+
+    def plot(self, force_replot=False, **kwargs):
         """
         an interface to be called publicly when doing specific plotting.
         DO NOT override this method directly, override the protected `_plot()` instead
