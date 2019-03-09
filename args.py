94f3df110b5cbd87b7dd072477cb054613a96f41
Shuheng Liu <wish1104@outlook.com>
2019-03-10 00:44:45 +0800

    register 3 new args for BaseDatasetArgs: `labels`, `balance`, `size`
    `labels` is used for filtering;
    `balance` is used for downsampling to ensure balanced classes;
    `size` is used for random downsampling.

--- args.py
+++ args.py
@@ -26,6 +26,9 @@ class BaseDatasetParser(_StaticParser):
         _StaticParser._setup(self)
         self.add_argument("--dataroot", default="../dataset", help="folder to hold dataset")
         self.add_argument("--datafile", default="emnist-byclass.mat", help="filename of dataset")
+        self.add_argument("--labels", default=None, help="specify certain labels to be used (all labels by default)")
+        self.add_argument("--balance", action="store_true", help="whether to down-sample to balance classes")
+        self.add_argument("--size", default=None, help="float, size of dataset to be used (1.0 by default)")
         return self
 
 
@@ -37,6 +40,22 @@ class BaseDatasetArgs(_StaticRegister):
     @property
     def datafile(self):
         return self.args.datafile
+
+    @property
+    def labels(self):
+        l = self.args.labels
+        if l is None:
+            return None
+        else:
+            return list(map(int, l.split()))
+
+    @property
+    def balance(self):
+        return self.args.balance
+
+    @property
+    def size(self):
+        return self.args.size
 
 
 class BaseModelParser(_StaticParser):
