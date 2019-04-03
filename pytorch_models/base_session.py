118889b586b30416b4944970e3d02a4789128f1f
Shuheng Liu <wish1104@outlook.com>
2019-04-03 17:33:07 +0800

    inherit AbstractSession, interfacing signatures of `epoch` and `step`
    handles logic of `to_report` in a dedicated property so that other methods don't have to do it

--- pytorch_models/base_session.py
+++ pytorch_models/base_session.py
@@ -1,12 +1,13 @@
 import torch
 from global_utils import flush_json_metrics
+from abstract_session import AbstractSession
 from preprocess import Subset
 from torch.utils.data import DataLoader
 from pytorch_models.torch_utils import prepend_tag, get_metrics_dict, EmbedModule
 from tensorboardX import SummaryWriter
 
 
-class ForwardSession:
+class ForwardSession(AbstractSession):
     def __init__(self, model, subset, batch, device, report_period=1):
         self.subset = subset  # type: Subset
         self.loader = DataLoader(subset, batch_size=batch, shuffle=True, num_workers=0)
@@ -15,6 +16,13 @@ class ForwardSession:
 
         self.device = device
         self._global_step = 0
+        self.verbose = True
+        self._name = "unnamed"
+
+    @property
+    def to_report(self):
+        # report metrics if self._verbose is set or at the end of each report_period
+        return self.verbose or (self._global_step + 1) % self.report_period == 0
 
     @property
     def global_step(self):
@@ -24,18 +32,14 @@ class ForwardSession:
     def n_samples(self):
         return len(self.subset)
 
-    def epoch(self, force_report=False):
+    def epoch(self):
         for samples_batch in self.loader:
-            # report metrics only at the end of each report_period
-            to_report = force_report or ((self._global_step + 1) % self.report_period == 0)
-            self.step(samples_batch, report=to_report)
+            self.step(samples_batch)
 
-    def step(self, samples_batch, report=True, tag="unspecified-tag"):
+    def step(self, samples_batch):
         """
         Feed forward 1 step and compute metrics (excluding loss), while increasing global_step by 1
         :param samples_batch: a batch yielded when iterating through a torch.utils.data.DataLoader
-        :param report: whether to report metrics or not
-        :param tag: tag of the step, usually 'train' or 'infer', used when reporting metrics
         :return: a tuple of (features, labels, logits, tagged_metrics)
         """
         self._global_step += 1
@@ -49,9 +53,9 @@ class ForwardSession:
             predictions = logits.max(1)[1]
             metrics = get_metrics_dict(labels, predictions)
 
-        tagged_metrics = prepend_tag(metrics, tag)
+        tagged_metrics = prepend_tag(metrics, self._name)
 
-        if report:
+        if self.to_report:
             self._report_metrics(tagged_metrics)
 
         return features, labels, logits, tagged_metrics
